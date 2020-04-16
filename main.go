6515f95a1e0f8150808889756971320dbb0dd92c
trendnchen <trendnchen@gmail.com>
2020-04-16 14:31:25 +1200

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World! Ning"}
+		res := &response{Message: "Hello World! Ning!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
